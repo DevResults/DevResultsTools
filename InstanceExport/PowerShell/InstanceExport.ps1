119797c824207b00168a5500e36c44600364af0a
Frederico Rodrigues <67288628+fredrodlima@users.noreply.github.com>
2021-06-01 18:15:06 -0400

    Handling PR suggestions

--- InstanceExport/PowerShell/InstanceExport.ps1
+++ InstanceExport/PowerShell/InstanceExport.ps1
@@ -4,14 +4,9 @@ param(
      [Parameter(Mandatory=$True, HelpMessage='Path where to create the exported files')][String] $exportFilePath,
      [Parameter(Mandatory=$True, HelpMessage='Your username on DevResults')][String] $userName,
      [Parameter(Mandatory=$True, HelpMessage='Your password on DevResults')][SecureString] $password,
-     [Parameter(Mandatory=$False, HelpMessage='Flag to override all files that already exists')][boolean] $override = $false
+     [Parameter(Mandatory=$False, HelpMessage='Flag to overwrite all files that already exists')][boolean] $overwrite = $false
 )
-$manifestJson = Get-Content -Raw -Path $manifestFilePath | ConvertFrom-Json
-$currentDate = Get-Date $manifestJson.CreateDate -Format "MM_dd_yyyy_HH_mm_ss"
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
-$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-
-function Write-ColorOutput($ForegroundColor)
+Function Write-ColorOutput($ForegroundColor)
 {
     # save the current color
     $fc = $host.UI.RawUI.ForegroundColor
@@ -33,25 +28,61 @@ function Write-ColorOutput($ForegroundColor)
 
 Function Log {
     param(
-        [Parameter(Mandatory=$true)][String]$msg
+        [Parameter(Mandatory=$true)][String]$msg,
+        [Parameter(Mandatory=$false)][String]$displayMsg,
+        [Parameter(Mandatory=$true)][String]$logLevel,
+        [Parameter(Mandatory=$true)][String]$currentDate
     )
-    $directoryPath = $exportFilePath
+    switch($logLevel)
+    {
+        "error"{
+            if ($displayMsg){
+                Write-ColorOutput red $displayMsg
+            }
+        }
+        "displayInfo"{
+            if ($displayMsg){
+                Write-ColorOutput yellow $displayMsg
+            }
+        }
+        "info"{
+            if ($displayMsg){
+                Write-Information $displayMsg
+            }
+        }
+        "success"{
+            if ($displayMsg){
+                Write-ColorOutput green $displayMsg
+            }
+        }
+    }
+    Add-Content "$($exportFilePath)/InstanceExport_$($currentDate)_log.txt" $msg
+}
+
+Function CreateDirectoryIfDoesNotExist{
+    param (
+        [Parameter(Mandatory=$true)][String]$directoryPath,
+        [Parameter(Mandatory=$true)][String] $currentDate
+    )
     $directoryExists = Test-Path -Path $directoryPath
     if (!$directoryExists)
     {
-        New-Item -Path $directoryPath -ItemType "directory" | Out-Null
-        Write-Information ($directoryPath + " created")
-        Log ($directoryPath + " created")
+        try{
+            New-Item -Path $directoryPath -ItemType "directory" | Out-Null
+            $msg = "$directoryPath created"
+            Log -msg $msg -displayMsg $msg -logLevel "info" -currentDate $currentDate
+        }
+        catch{ 
+            $msg = "Error trying to create $directoryPath directory"
+            $displayMsg = "Error creating directory"
+            Log -msg $msg -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
+        }
     }
-    Add-Content ($directoryPath + "/InstanceExport_" + $currentDate +"_log.txt") $msg
 }
 
-function Export
-{
-<#
-.Description
-This function exports an given instancce and save it to your disk in the specified exportFilePath from a JSON manifest file
-#>    
+Function Login(){
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
     $Body = @{
         UserName = $userName
@@ -60,102 +91,142 @@ This function exports an given instancce and save it to your disk in the specifi
 
     $Parameters = @{
         Method = "POST"
-        Uri = "https://" + $manifestJson.SubDomain + "." + $manifestJson.HostName + "/api/login"
+        Uri = "https://$($manifestJson.SubDomain).$($manifestJson.HostName)/api/login"
         Body = ($Body | ConvertTo-Json)
         ContentType = "application/json"
     }
 
-    try{
-        $accessTokenResponseModel = Invoke-RestMethod @Parameters
+    $accessTokenResponseModel = Invoke-RestMethod @Parameters
+    
+    return $accessTokenResponseModel
+}
 
-        $accessToken = $accessTokenResponseModel.access_token
+function Export
+{
+<#
+.Description
+This function exports an given instancce and save it to your disk in the specified exportFilePath from a JSON manifest file
+#>    
+    $manifestFileExists = Test-Path -Path $manifestFilePath
 
-        $Header = @{
-            "authorization" = "Bearer $accessToken"
+    if ($manifestFileExists)
+    {
+        $manifestJson = Get-Content -Raw -Path $manifestFilePath | ConvertFrom-Json
+        if ($manifestJson.CreateDate){
+            $currentDate = Get-Date $manifestJson.CreateDate -Format "MM_dd_yyyy_HH_mm_ss"
+        }
+        else{
+            $msg = "Error in reading manifest file"
+            $displayMsg = "An error occurred when reading manifest file"
+            Log -msg $msg -displayMsg $displayMsg -logLevel "error"  -currentDate $currentDate
+        }
+    
+        try{
+            CreateDirectoryIfDoesNotExist -directoryPath $exportFilePath -currentDate $currentDate
+        }
+        catch{
+            $msg = "Error trying to create $directoryPath directory"
+            $displayMsg = "An error occurred when trying to create a new directory"
+            Log -msg $msg -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
         }
 
-        try
-        {
+        try{
+            $accessTokenResponseModel = Login
+        }
+        catch{
+            Log -msg "An error occurred" -displayMsg "$($_.Exception.Response.StatusCode.value__): An error occurred when logging in" -logLevel "error" -currentDate $currentDate
+            Log -msg "Authorization error for Instance Export" -logLevel "error" -currentDate $currentDate
+            Log -msg "StatusCode: $($_.Exception.Response.StatusCode.value__)" -logLevel "error" -currentDate $currentDate
+            Log -msg "Url: api/login" -logLevel "error" -currentDate $currentDate
+            Log -msg "StatusDescription: $($_.Exception.Response.StatusDescription)" -logLevel "error" -currentDate $currentDate
+        }
+
+        if ($accessTokenResponseModel){
+
+            $accessToken = $accessTokenResponseModel.access_token
+
+            $Header = @{
+                "authorization" = "Bearer $accessToken"
+            }
+
             $currentCategory = "";
             foreach ($entry in $manifestJson.Entries)
             {
                 if ($currentCategory -ne $entry.Category)
                 {
                     $currentCategory = $entry.Category;
-                    Write-ColorOutput yellow ("Starting extraction of " + $entry.Category + " category.")
-                    Log ("Starting extraction of " + $entry.Category + " category.")
+                    $message = "Starting extraction of $($entry.Category) category."
+                    Log -msg $message -displayMsg $message -logLevel "displayInfo" -currentDate $currentDate
                 }
-                
-                Write-Information yellow ("Extracting data from " + $entry.Url)
-                Log ("Extracting data from " + $entry.Url)
+                $message = "Extracting data from $($entry.Url)"
+                Log -msg $message -displayMsg $message -logLevel "displayInfo" -currentDate $currentDate
                 $directoryPath = $exportFilePath + "/" + $entry.Path
-                $directoryExists = Test-Path -Path $directoryPath
-                if (!$directoryExists)
-                {
-                    New-Item -Path $directoryPath -ItemType "directory" | Out-Null
-                    Write-Information ($directoryPath + " created")
-                    Log ($directoryPath + " created")
+            
+                try{
+                    CreateDirectoryIfDoesNotExist -directoryPath $directoryPath -currentDate $currentDate
                 }
-
-                $filePath =  $directoryPath + "/" + $entry.FileName
+                catch{
+                    $msg = "Error trying to create $directoryPath directory"
+                    $displayMsg = "An error occurred when trying to create a new directory"
+                    Log -msg $msg -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
+                }
+                $filePath = "$directoryPath/$($entry.FileName)"
                 $fileAlreadyExists = Test-Path -Path $filePath -PathType Leaf
-
-                if (!$fileAlreadyExists -or $override)
+                if (!$fileAlreadyExists -or $overwrite)
                 {
-                    if ($entry.Url.Contains("/content") -or $entry.Url.Contains("xlsx") -or $entry.Url.Contains("/template")){
-                        $entryExportParameters = @{
-                            Method      = "GET"
-                            Uri         = "https://" + $manifestJson.SubDomain + "." + $manifestJson.HostName + $entry.Url
-                            Headers     = $Header
-                            ContentType = "application/json"  
-                        }
+                    $entryExportParameters = @{
+                        Method      = "GET"
+                        Uri         = "https://$($manifestJson.SubDomain).$($manifestJson.HostName)$($entry.Url)"
+                        Headers     = $Header
+                        ContentType = "application/json"  
+                    }
                         
-                        try{
-                            $GetEntriesResponse = Invoke-RestMethod @entryExportParameters -OutFile $filePath
+                    try{
+                        if (!$entry.fileName.Contains(".json")){
+                            Invoke-RestMethod @entryExportParameters -OutFile $filePath | Out-Null
                         }
-                        catch{
-                            $message = "Error occured when extracting data from " + $entry.Url
-                            Write-ColorOutput red ($message)
-                            Log ($message)
+                        else{
+                            $GetEntriesResponse = Invoke-RestMethod @entryExportParameters
+                            $GetEntriesResponse | ConvertTo-Json -Depth 100 | Out-File -FilePath $filePath
                         }
                     }
-                    else{
-                        $entryExportParameters = @{
-                            Method      = "GET"
-                            Uri         = "https://" + $manifestJson.SubDomain + "." + $manifestJson.HostName + $entry.Url
-                            Headers     = $Header
-                            ContentType = "application/json"   
-                        }
-                        try{
-                            $GetEntriesResponse = Invoke-RestMethod @entryExportParameters
-
-                            $GetEntriesResponse | ConvertTo-Json  | Out-File -FilePath $filePath
-                        }
-                        catch{
-                            $message = "Error occured when extracting data from " + $entry.Url
-                            Write-ColorOutput red ($message)
-                            Log ($message)
-                        }
+                    catch{
+                        $message = "Error occurred when extracting data from $($entry.Url)"
+                        Log -msg "An error occured" -displayMsg $message -logLevel "error" -currentDate $currentDate
+                        Log -msg $message -logLevel "error" -currentDate $currentDate
+                        Log -msg "StatusCode: $($_.Exception.Response.StatusCode.value__)" -logLevel "error" -currentDate $currentDate
+                        Log -msg "Url: $($entry.Url)" -logLevel "error" -currentDate $currentDate
+                        Log -msg "StatusDescription: $($_.Exception.Response.StatusDescription)" -logLevel "error" -currentDate $currentDate
                     }
                 }
                 else{
-                    $message = "Skipping " + $filePath + " because file already exists"
-                    ## This can be uncommented for debug purposes
-                    # Write-ColorOutput white ($message)
-                    Log ($message)
+                    $message = "Skipping $filePath because file already exists"
+                    Log -msg $message -displayMsg $message -logLevel "info" -currentDate $currentDate
                 }
             }
+            $msg = "Exporting Instance run finished."
+            Log -msg $msg -displayMsg $msg -logLevel "success" -currentDate $currentDate
+        }
+        else{
+            $msg = "Exporting Instance run finished with errors."
+            $displayMsg = "Exporting Instance run was not succesfull. Check the log file for more details."
+            Log -msg $msg -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
+        }
+        
+    }
+    else{
+        $currentDate = Get-Date -Format "MM_dd_yyyy_HH_mm_ss"
+    
+        try{
+            CreateDirectoryIfDoesNotExist -directoryPath $exportFilePath -currentDate $currentDate
         }
         catch{
-            Write-ColorOutput red "Authorization error for Instance Export"
-            Log "Authorization error for Instance Export"
+            $displayMsg = "An error occurred when trying to create a new directory"
+            Log -msg "Error trying to create $directoryPath directory" -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
         }
-        Write-ColorOutput green "Exporting Instance finished."
-        Log "Exporting Instance finished."
-    }
-    catch{
-        Write-ColorOutput red "An error occurred when logging in"
-        Log "Authorization error for Instance Export"
+
+        $displayMsg = "An error occurred when looking for manifest file"
+        Log -msg "Error in finding manifest file" -displayMsg $displayMsg -logLevel "error" -currentDate $currentDate
     }
 }
 Export
--- InstanceExport/PowerShell/README.md
+++ InstanceExport/PowerShell/README.md
@@ -1,61 +1,38 @@
 ## Instance Export Power Shell
 
-At DevResults we value the concept that the data is yours and you have rights of getting snapshots of it at anytime. For that reason,
-we have created a tool to make it possible for you to export and save all data you have at DevResults on your own machines.
+At DevResults we value the concept that the data is yours and you have rights of getting snapshots of it at anytime. For that reason, we have created a tool to make it possible for you to export and save all data you have at DevResults on your own machines.
 
 In order to use it, you should:
 
 1. Download the InstanceExport power shell script available in this repo to your machine.
 
-2. Use our API to make a POST request an updated Instance Export Manifest:
+2. Use DevResults API to make a POST request an updated Instance Export Manifest:
  ```POST https://{myInstanceName}.devresults.com/api/currentinstance/export```
 
 where {myInstanceName} is the name of your site in DevResults
 
 3. Save the file in the same directory you have saved the powershell script. It's important to save the file in the JSON format and that you keep in mind the name you saved the file. In this tutorial the name we used is *manifest.json*
 
-4. Open a new command line interface (CLI) prompt that supports using PowerShell commands. If you don't have PowerShell installed you can follow instructions at [Installing Power Shell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1)
+4. Open a new command line interface (CLI) prompt that supports using of PowerShell commands. If you don't have PowerShell installed you can follow instructions at [Installing Power Shell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1)
 
 5. Navigate to the directory you have saved the InstanceExport.ps1 and the JSON manifest file.
 
 e.g.: cd C:\Users\MyUser\InstanceExport
 
-6. In the cli prompt create the following variables
-    ```
-    $manifestFilePath="{manifestPathFileName}"
-    ```
-> PS.: In this turorial example is manifest.json because it is in same folder of the PS script
-
-    ```
-    $exporttFilePath="{exportFilePath}"
-    ```
-> where {exportFilePath} should be replace with path where you want to save the exported files. In this example case we have used "C:/InstanceExport" but you can define any other if you wish.
-
-    ```
-    $userName="{myuser}@devresults.com"
-    ```
-> where {myUser} should be replaced by your username at your DevResults site to login
-
-    ```
-    $password="{myPassword}"
-    ```
-> where {myPassword} should be replaced by your password at your DevResults site to login
-
-    ```
-    $encryptedPassword = ConvertTo-SecureString $password -AsPlainText -Force
-    ```
-
-7. After creating all those variables you can begin exporting your data using the following command
+6. In the cli prompt type the following command
    
    ```
-   .\InstanceExport -manifestFilePath $manifestFilePath -exportFilePath $exportFilePath -userName $userName -password $encryptedPassword -override $true
+   .\InstanceExport
    ```
 
-The process will run automatically and it will propmt the progress of yourdata been exported by each available category. When all is finishedd you should see a message "Exporting Instance finished"
+The process will prompt to you the mandatory fields to be typed in your screen $manifestFilePath, $exportFilePath, $userName and $password. 
+It will run automatically after you enter the fields and it will propmt the progress of your data been exported by each available category. When all is finishedd you should see a message "Exporting Instance finished"
 
-The created power shell script has four parameters that are explained below:
+The created power shell script has five parameters that are explained below:
 - manifestFilePath : Path of the manifest file you have downloaded using step 2
-- exportFilePath: Path to create the folder for the export files
+- exportFilePath: Path to create the folder for the exported files
 - userName: Your username for login at DevResults
-- password: Your password, should be a encrypted string (see step 6), for login at DevResults
-- override: Optional parameter to inform if you will override files that already exist and replace them. If you don't use the value of it will be false, which means that if a file already exists in the exportFilePath it will be skipped.
+- password: Your password for login at DevResults or API Key's Secret
+- overwrite: Optional parameter to inform if you want to overwrite files that already exist and replace them. If you don't use the value of it will be false, which means that if a file already exists in the exportFilePath it will be skipped.
+
+PS.: Alternatively, you can right click in the InstanceExport.ps1 file, choose "run with powershell" and follow the prompst too.
