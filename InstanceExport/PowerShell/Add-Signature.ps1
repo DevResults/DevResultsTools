8b0278f18c468a4e9979e3b7a4acd4932ee5cc05
Frederico Rodrigues <fred@devresults.com>
2023-01-18 17:33:19 -0500

    script: Updating Add-Signature script
    In next commit I'm going to try to use the steps NG have done for Connectors repo to sign the ps script for InstanceExport

--- InstanceExport/PowerShell/Add-Signature.ps1
+++ InstanceExport/PowerShell/Add-Signature.ps1
@@ -1,4 +1,4 @@
 ## Signs a file
 param([string] $file=$(throw "Please specify a filename."))
-$cert = @(Get-ChildItem cert:\CurrentUser\My -codesigning)[0]
+$cert = @(Get-ChildItem cert:\LocalMachine\My -codesigning)[0]
 Set-AuthenticodeSignature $file $cert
